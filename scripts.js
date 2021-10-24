f261c4e73e2cd32a7c8edf8c74dd0be016609402
Rupert Scott <rupertscott1706@gmail.com>
2021-10-23 19:28:41 -0500

    Added A Sprinkle of Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+
+    var my_func = function(event) {
+    event.preventDefault();
+    // window.history.back();
+    var email = document.getElementById("email").value;
+    console.log(email);
+    var output = document.getElementsByClassName("message")[0];
+    if(email != ""){
+        output.innerHTML = "Thank You! Your email " + email + " has been added to our mailing list!";
+    }else{
+        output.innerHTML = "Please enter valid email address";
+    }
+    
+};
+
+    var butn = document.getElementsByTagName("button")[0];
+
+    butn.addEventListener("click", my_func, true);
+}
